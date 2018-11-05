ec9759c9374c60370a91e1d9a3f5cbad06bcab67
Gaojie Wang <wanggaojie913@yahoo.com>
2018-11-05 19:29:43 +0000

    selections

--- stringFunc.sql
+++ stringFunc.sql
@@ -37,3 +37,34 @@ FROM books ORDER BY 2;
  
 SELECT author_fname, author_lname FROM books 
 ORDER BY author_lname, author_fname;
+
+ 
+SELECT title FROM books WHERE  title LIKE 'the';
+ 
+SELECT title FROM books WHERE  title LIKE '%the';
+ 
+SELECT title FROM books WHERE title LIKE '%the%';
+
+SELECT title, stock_quantity FROM books WHERE stock_quantity LIKE '____';
+ 
+SELECT title, stock_quantity FROM books WHERE stock_quantity LIKE '__';
+ 
+(235)234-0987 LIKE '(___)___-____'
+ 
+SELECT title FROM books;
+ 
+SELECT title FROM books WHERE title LIKE '%\%%';
+ 
+SELECT title FROM books WHERE title LIKE '%\_%';
+
+SELECT title, author_lname FROM books WHERE author_lname LIKE '% %';
+
+SELECT
+    CONCAT(
+        'MY FAVORITE AUTHOR IS ',
+        UPPER(author_fname),
+        ' ',
+        UPPER(author_lname),
+        '!'
+    ) AS ShoutOut
+FROM books ORDER BY author_lname;
