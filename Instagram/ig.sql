e491c4ad7787b40198bb7f48ce83c3155a25c239
Gaojie Wang <wanggaojie913@yahoo.com>
2018-12-15 03:57:08 +0000

    M

--- Instagram/ig.sql
+++ Instagram/ig.sql
@@ -14,3 +14,30 @@ FROM users
 GROUP BY day
 ORDER BY total;
     
+    
+SELECT 
+    users.id,
+    username,
+    user_id
+FROM users
+LEFT JOIN photos
+    ON users.id = photos.user_id
+WHERE user_id IS NULL;
+
+SELECT 
+    username,
+    photos.id,
+    photos.image_url, 
+    COUNT(*) AS total
+FROM photos
+INNER JOIN likes
+    ON likes.photo_id = photos.id
+INNER JOIN users
+    ON photos.user_id = users.id
+GROUP BY photos.id
+ORDER BY total DESC
+LIMIT 1;
+
+SELECT 
+    (SELECT Count(*) FROM   photos) / (SELECT Count(*) FROM   users) 
+    AS avg; 
